58d61e11682296380df6c31cf1059b5cdd39329a
aquarion <nicholas@aquarionics.com>
2008-05-27 18:28:46 +0000

    Database changes

--- schema.sql
+++ schema.sql
@@ -1,3 +1,4 @@
-create table hugReaction (username varchar(64), item varchar(255), primary key(username));
-create table lastseen (username varchar(64), last_seen float, last_words varchar(255), primary key(username));
-create table urllist (username varchar(64), time float, message varchar(255));
+CREATE TABLE hugReaction (username varchar(64), item varchar(255), primary key(username));
+CREATE TABLE lastseen (username varchar(64), last_seen float, last_words varchar(255), primary key(username));
+CREATE TABLE urllist (username varchar(64), time float, message varchar(255));
+CREATE TABLE vote (id int, item varchar(64), vote_count tinyint);
