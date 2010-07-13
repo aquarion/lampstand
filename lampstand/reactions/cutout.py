958904f84cacafd97d4f07dba46e82d443c8296e
Nicholas Avenell <nicholas@aquarionics.com>
2010-07-13 11:04:21 +0100

    Add 'your line was too long' notification

---
+++ lampstand/reactions/cutout.py
@@ -0,0 +1,34 @@
+import lampstand.reactions.base
+
+
+from lampstand.tools import splitAt
+import re, time, random, sys
+
+def __init__ ():
+	pass
+
+class Reaction(lampstand.reactions.base.Reaction):
+
+
+	__name = 'Cutout'
+	
+	def everyLine(self, connection, user, channel, message):
+
+		if len(message) > 450:
+			print "Looks like a cutout line"
+	                index = len(message)-1
+			words = 0
+			maxword = 5
+	                whitespace =  re.compile('\s')
+
+			while words < maxword and index > 400:
+				if whitespace.match(message[index]):
+					words = words + 1
+				index = index -1
+
+			index = index + 2;
+
+			cutout = message[index:]
+			connection.msg(channel, "%s: I think that cut out at \"%s\"" % (user, cutout));
+
+			return True
