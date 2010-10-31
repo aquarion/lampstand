a586862960ccd5a41778848be755c59914ec49a9
Nicholas Avenell <nicholas@aquarionics.com>
2010-10-31 21:54:21 +0000

    Nanowrimo nag support

---
+++ lampstand/reactions/nano.py
@@ -0,0 +1,52 @@
+import re, time, random, sys, math
+import lampstand.reactions.base
+from lampstand import tools
+import datetime
+
+def __init__ ():
+	pass
+
+class Reaction(lampstand.reactions.base.Reaction):
+	
+	__name = 'Nano'
+	
+	cooldown_number = 2
+	cooldown_time   = 360
+	uses = []
+
+	def __init__(self, connection):
+		self.channelMatch = re.compile("^%s. how many words" % connection.nickname, re.IGNORECASE)
+		self.privateMatch = re.compile("^how many words", re.IGNORECASE)
+
+	def channelAction(self, connection, user, channel, message):
+		connection.msg(channel, "%s: %s" % (user, self.nano() ))
+	
+	def privateAction(self, connection, user, channel, message):
+		connection.msg(user, self.nano() )
+	
+	def nano(self):
+		print "[Nano] called"
+	
+		now = time.time()
+
+		then = time.mktime(datetime.datetime(datetime.datetime.now().year, 11, 1, 00, 00).timetuple() )
+
+		delta = now - then;
+
+		target = 50000.0;
+		month  = 2592000.0;
+
+		end = then+month;
+
+		if now > then+month:
+			return "You should have finished by now"
+
+		if delta < 0:
+			delta = math.fabs(delta)
+			return "You should start Nanowrimo in %s" % tools.niceTimeDelta(delta, "decimal")
+
+		persec = target/month;
+
+		current = delta*persec;
+
+		return "You should be at %d words by now, %s left" % (current, tools.niceTimeDelta(month-delta));	
