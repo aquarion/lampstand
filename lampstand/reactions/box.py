1403519012c72f1bc7aa7e25ca9fc25c89d847e3
Nicholas Avenell <nicholas@aquarionics.com>
2011-07-02 18:51:48 +0100

    Disabled boxes temporarily

--- lampstand/reactions/box.py
+++ lampstand/reactions/box.py
@@ -23,6 +23,9 @@ class Reaction(lampstand.reactions.base.Reaction):
 			connection.msg(channel, "I'm out of boxes, new delivery shortly.")
 			return
 
+		connection.msg(channel, "Sorry, %s, the warehouse is on strike, and boxes aren't available" % user)
+		return
+		
 		print "[Box] called "
 
 		level = random.randint(1,5);
