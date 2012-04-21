8728bf64dd715bd9da779654048a37245fb5fe03
Nicholas Avenell <nicholas@aquarionics.com>
2012-04-21 01:22:29 +0100

    [CORE] Baby steps towards UTF-8

--- lampstand.py
+++ lampstand.py
@@ -398,7 +398,7 @@ class LampstandLoop(irc.IRCClient):
 		self.logger.log("* %s %s" % (user, msg))
 
 	def message(self, user, message, length=200):
-		return self.msg(user,message,length)
+		return self.msg(user,message.encode("utf-8"),length)
 
 	# irc callbacks
 
