21c83f28d0178ce6883b74aada42966666d2a757
Nicholas Avenell <nicholas@aquarionics.com>
2012-05-16 09:15:47 +0100

    [Boxes] Reenabled boxes

--- lampstand/reactions/box.py
+++ lampstand/reactions/box.py
@@ -2,6 +2,7 @@ import re, time, urllib, sys
 import random
 import lampstand.reactions.base
 from lampstand.tools import splitAt
+import simplejson
 
 def __init__ ():
 	pass
@@ -9,8 +10,8 @@ def __init__ ():
 class Reaction(lampstand.reactions.base.Reaction):
 	__name = 'Box'
 
-	cooldown_number   = 5
-	cooldown_time     = 600
+	cooldown_number   = 2
+	cooldown_time     = 300
 	uses              = []
 
 	def __init__(self, connection):
@@ -23,40 +24,17 @@ class Reaction(lampstand.reactions.base.Reaction):
 			connection.msg(channel, "I'm out of boxes, new delivery shortly.")
 			return
 
-		connection.msg(channel, "Sorry, %s, the warehouse is on strike, and boxes aren't available" % user)
-		return
-		
 		print "[Box] called "
 
-		level = random.randint(1,5);
 
+		box = simplejson.load(urllib.urlopen("http://warehousebasement.com/api.php"))
 
-		sock = urllib.urlopen('http://www.warehouse23.com/basement/box/index.html?level=%d' % level)
-		box = sock.read() 
+		print box
 
-		description = re.findall('<p>\n(.*)\n</p>', box, re.MULTILINE)[0];		
+		description = ' '.join(box['description'].splitlines())
 
-		result = 'In a box on level %d there is %s ' % (level, description)
+		result = 'In a box on level %d there is %s ' % (1, description)
 
-		result = ' '.join(result.split('\n'))
-
-		result = re.sub(r'<[^>]*?>', '', result) 
-
-		self.updateOveruse()
-
-		if len(result) > 880*2:
-
-			whereToSplit = splitAt(result, 860)
-			result = "%s [Cut for length]" % result[0:whereToSplit]
-
-		if len(result) > 440:
-			whereToSplit = splitAt(result, 440)
-			stringOne = result[0:whereToSplit]
-			stringTwo = result[whereToSplit:]
-
-			connection.msg(channel, "%s... " % stringOne)
-			connection.msg(channel, "... %s" % stringTwo)
-		else:
-			connection.msg(channel, "%s" % result)
+		connection.message(channel, "%s" % result)
 
 		return True
