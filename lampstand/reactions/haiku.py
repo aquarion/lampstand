62622ff612a0ee8cc84891d6b68f3dbbfa0b5146
Nicholas Avenell <nicholas@aquarionics.com>
2012-09-28 12:48:37 +0100

    [Haiku] Maintain syllables dictionary between restarts, and clean up responses a bit

--- lampstand/reactions/haiku.py
+++ lampstand/reactions/haiku.py
@@ -3,6 +3,7 @@ import re, time, random, sys, datetime
 import lampstand.reactions.base
 from lampstand import tools
 import haikufinder
+import cPickle  as pickle
 
 def __init__ ():
 	pass
@@ -19,13 +20,14 @@ class Reaction(lampstand.reactions.base.Reaction):
 		self.getconfig(connection)
 		
 		print "Haiku Init!"
-		self.channelMatch = (re.compile('%s. how many syllables in (\w*)\??' % connection.nickname, re.IGNORECASE),
+		self.channelMatch = (re.compile('%s. how many syllables in ([\w ]*)\??' % connection.nickname, re.IGNORECASE),
 			re.compile('%s. (\w*) has (\d\d?) syllables?\.?' % connection.nickname, re.IGNORECASE)
 			)
 		self.dbconnection = connection.dbconnection
 		
 		self.channels = {}
-		
+		self.haikus   = {}
+		self.load()
 
 	def channelAction(self, connection, user, channel, message, matchindex):
 				
@@ -61,9 +63,28 @@ class Reaction(lampstand.reactions.base.Reaction):
 		else:
 			ordinal = "s"
 		
-		connection.msg(channel, "%s: %s has %s syllable%s" % (user, matches[0], syllables, ordinal))
+		connection.msg(channel, "%s: \"%s\" has %s syllable%s" % (user, matches[0], syllables, ordinal))
 		return True
-	
+
+	def save(self):
+		print "[Haiku] Saving database..."
+		output = open("haiku_cheatsheet.pkl.db", 'wb')
+		pickle.dump(self.cheatsheet, output)
+		output.close()
+
+	def load(self):
+		print "[Haiku] Loading database..."
+		try:
+			input = open("haiku_cheatsheet.pkl.db", 'rb')
+			self.cheatsheet = pickle.load(input)
+			input.close()
+			for word, count in self.cheatsheet:
+				print "[Haiku] %s has %d syllables" % (word, int(count))
+				haikufinder.HaikuFinder.add_word(word, int(count));
+		except:
+			self.cheatsheet = {}
+
+
 	def cheatSheet(self, connection, user, channel, message):
 		print "Cheatsheet"
 		
@@ -71,7 +92,12 @@ class Reaction(lampstand.reactions.base.Reaction):
 		matches = self.channelMatch[1].findall(message)
 		
 		haikufinder.HaikuFinder.add_word(matches[0][0], int(matches[0][1]));
-		
+
+		self.cheatsheet[matches[0][0]] = int(matches[0][1]);
+
+		self.save()
+				
+
 		connection.msg(channel, "%s: If you say so" % user)
 		
 		return True
@@ -101,6 +127,7 @@ class Reaction(lampstand.reactions.base.Reaction):
 				
 			connection.msg(channelout, "That looked like a haiku on %s: %s " % (channel, " // ".join(haikus[0])))
 			self.channels[channel] = []
+			self.lasthaiku[channel] = " // ".join(haikus[0])
 			return True
 		
 		return False
