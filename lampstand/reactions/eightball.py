73020cf4bfb5eca35609f541c5b9a49b80159b75
Nicholas Avenell <nicholas@aquarionics.com>
2012-10-15 18:57:18 +0100

    Join eightball resources into single file

--- lampstand/reactions/eightball.py
+++ lampstand/reactions/eightball.py
@@ -1,6 +1,4 @@
-from lampstand import eightball
 import lampstand.reactions.base
-
 
 from lampstand.tools import splitAt
 import re, time, random, sys
@@ -37,6 +35,72 @@ class Reaction(lampstand.reactions.base.Reaction):
 
 		self.updateOveruse()
 		
-		connection.message(channel, "%s: %s" % (user, eightball.question()) )
+		connection.message(channel, "%s: %s" % (user, self.question()) )
 
 		return True
+
+
+	def question(self):
+		answers = (
+			"As I see it, yes",
+			"Ask again later",
+			"Cannot predict now",
+			"Don't count on it",
+			"It is certain",
+			"It is decidedly so",
+			"Most likely",
+			"My reply is no",
+			"My sources say no",
+			#"Outlook good",
+			#"Outlook not so good",
+			"Reply hazy, try again",
+			"Signs point to yes",
+			"Very doubtful",
+			"Without a doubt",
+			"Yes",
+			"Yes - definitely",
+			"You may rely on it",
+			#"Your answer lies within the catbus",
+			#"Only Legion can help you now",
+			#"The maelstrom deems it unlikely",
+			"Transparent Electronic Envelopes. I think that means No",
+			"Probably",
+			#"The internal kittens say 'No'. Also 'Keep Aestar Away From Us'",
+			"Nonspecifically, yes",
+			"YES! YES! A THOUSAND TIMES YES",
+			"Not until you're all under my domain",
+			"I shall not dignify your question with an answer",
+			#"No idea. Shall I phone a friend?",
+			#"Over my dead body",
+			"Hah. No",
+			"Hah. Yes",
+			#"Consecrate and ask again",
+			#"An Eidolon will be around shortly to answer",
+			#"Go ask someone who gives a damn",
+			"Purple hatstand fur elysian triumverate",
+			"Of course not, don't be silly",
+			#"Ask eliza",
+			#"Supplicate and see",
+			"Not in this universe. Try another",
+			#"As soon as downtime is open",
+			#"Supplicate and see what happens",
+			"Pray and see what happens",
+			"Not this time",
+			"Only once",
+			"yes, but bricks will fall after",
+			#"Pah, do not bother me with such trifles",
+			"A suffusion of yellow",
+			#"Ask your mother",
+			#"Go ask your father",
+			#"Go play in the traffic",
+			"Go play with a triffid",
+			"Why ask questions you know the answer to?",
+			"I'm going to say yes, but only because the results will be funnier",
+			"That would be a 'No'",
+			"No",
+			"Yes, but only if you put chocolate sprinkles on it"
+
+			)
+		
+		#return answers[(random.randint(0,len(answers)))]
+		return random.choice(answers)
